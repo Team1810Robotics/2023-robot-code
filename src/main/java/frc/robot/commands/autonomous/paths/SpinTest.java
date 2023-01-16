0c5783414e2a3ca7ae96fa96ecaaea82d76a05d9
Jack_W() <willijoh005@usd232.org>
2023-01-15 23:12:09 -0600

    New test path

---
+++ src/main/java/frc/robot/commands/autonomous/paths/SpinTest.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.autonomous.paths;
+
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.autonomous.FollowPath;
+import frc.robot.subsystems.DriveSubsystem;
+
+import static frc.robot.Constants.*;
+
+// 5 meter path that rotates 180*
+public class SpinTest extends SequentialCommandGroup {
+    public SpinTest(DriveSubsystem driveSubsystem) {
+        PathPlannerTrajectory trajectory1 = PathPlanner.loadPath("spinTest",
+            AutoConstants.MAX_SPEED_METERS_PER_SECOND,
+            AutoConstants.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED);
+
+        addCommands(
+            new InstantCommand(() -> driveSubsystem.zeroGyro()),
+            new FollowPath(trajectory1, driveSubsystem)
+        );
+    }
+}
